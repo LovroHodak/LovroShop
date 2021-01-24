a45f44ffaad868801944e9be5c85d39155de9eaa
lovro <hodak.lovro@gmail.com>
2021-01-24 17:39:20 +0100

    redux - product

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,10 +2,15 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+//reducers
 import { cartReducer } from './reducers/cartReducers'
+import { getProductDetailsReducer, getProductsReducer } from './reducers/productReducers'
+
 
 const reducer = combineReducers({
-    cart: cartReducer
+    cart: cartReducer,
+    getProducts: getProductsReducer,
+    getProductDetails: getProductDetailsReducer
 })
 
 const middleware = [thunk]
